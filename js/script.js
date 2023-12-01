7bbdc675659cc24225212f8ef59fa392439e4703
XklienZ <flince123@gmail.com>
2023-12-01 22:55:10 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const navbar = document.querySelector(".navbr-nv");
 const listIcon = document.getElementById("list-icon");
+const menuCard3 = document.querySelector(".menu-card3");
+const floatBtn = document.querySelector(".float-btn");
+const floatContainer = document.querySelector(".float-container");
+const floatOverlay = document.querySelector(".float-overlay");
 
 listIcon.onclick = () => {
   navbar.classList.toggle("active");
 };
 
-document.addEventListener("click", function (e) {
+floatBtn.onclick = () => {
+  floatContainer.classList.toggle("active");
+};
+
+function closeFloat(e) {
   if (!listIcon.contains(e.target) && !navbar.contains(e.target)) {
     navbar.classList.remove("active");
   }
+  if (!floatContainer.contains(e.target) && !floatBtn.contains(e.target)) {
+    floatContainer.classList.remove("active");
+  }
+}
+document.addEventListener("click", function (e) {
+  closeFloat(e);
 });
